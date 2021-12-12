f105325e37a7d88bed28eff25a9ac1965dd70afb
Breanna Richards <brichards21@amherst.edu>
2021-12-12 03:22:57 -0500

    push updated and commented get_diagnosed and run_animation functions

--- get_diagnosed.R
+++ get_diagnosed.R
@@ -4,14 +4,29 @@ get_diagnosed <- function(e = 477, i = 286, q = 191,
                           lambda = (1/3)*(3/5), delta = (15/100)*(1/21), 
                           theta = 1/3, sigma = 1/3, gamma = 1/21, k = 0.1, b = (31 + t)/(22 + 5*t)) {
   
-  #' get_diagnosed
+#' get_diagnosed generates epidemic data with exposed, infected, quarantined, diagnosed, and recovered individuals for 400 days
+#'@param e initial number of exposed
+#'@param i initial number of infectives
+#'@param q inital number of quarantined
+#'@param j initial number of diagnosed
+#'@param r initial number of recovered
+#'@param epsilon rate of exposed people moving into infective class
+#'@param lambda rate of exposed people moving into quarantined class
+#'@param delta rate of infected and diagnosed people dying
+#'@param theta rate of infectives moving into diagnosed class
+#'@param sigma rate of quarantined people moving into diagnosed class
+#'@param gamma rate of diagnosed moving into recovered class
+#'@param k infectivity fraction for the exposed individuals compared with individuals in the infective class
+#'@param b transmission rate
+#'@return all_data data frame containing number of individuals in each population class
+
   
-  days <- 400
+  days <- 400 # collect data for 400 days
   
   
   for (t in 1:(days - 1)) {
     
-    
+  # compute number of exposed, infected, quarantined, diagnosed, and recovered each day  
     e[t+1] <- e[t] + b*((k*e[t])+i[t]) - (epsilon+lambda)*e[t]
     i[t+1] <- i[t] + epsilon*e[t] - (delta + theta)*i[t]
     q[t+1] <- q[t] + lambda*e[t] - sigma*q[t]
@@ -20,6 +35,8 @@ get_diagnosed <- function(e = 477, i = 286, q = 191,
     
     
     if (b == (31 + t)/(22 + 5*t)) {
+    
+    # update transmission rate if transmission rate is default
       
       b <- (31 + (t+1))/(22 + 5*(t+1))
       
@@ -29,9 +46,12 @@ get_diagnosed <- function(e = 477, i = 286, q = 191,
     
   }
   
+  # combine population data into one data frame
+  
   all_data <- data.frame(cbind(Time = 1:days, Exposed = e, Infectives = i, 
                                Quarantined = q, Diagnosed = j, Recovered = r))
   
+  # transform data into longer format for plotting purposes
   
   all_data <- all_data %>%
     pivot_longer(!Time, names_to = "people_type", values_to = "num_people")
--- run_animation.R
+++ run_animation.R
@@ -1,9 +1,15 @@
 run_animation <- function(df_use) {
   
-  wide_dat <- df_use %>%
-    pivot_wider(names_from = "people_type", values_from = "num_people")
+#' run_animation displays an interactive plot 
+#'@param df_use filtered data set based on user input
+#'@return fig interactive plot
   
   accumulate_by <- function(dat, var) {
+#' accumulate_by accumulates observations for each value of var to create the frame variable to be used by an interactive plot
+#'@param dat data frame
+#'@param var variable to accumulate by 
+#'@return accumulated data with frame variable
+
     var <- lazyeval::f_eval(var, dat)
     lvls <- plotly:::getLevels(var)
     dats <- lapply(seq_along(lvls), function(x) {
@@ -14,9 +20,10 @@ run_animation <- function(df_use) {
   }
   
   
-  df_use <- accumulate_by(df_use, ~ Time) 
+  df_use <- accumulate_by(df_use, ~ Time) # accumulate data by time to create frames for interactive plot
   
   
+  # form basic ggplot and separate lines by the population types (i.e. Exposed, Infectives, etc.) selected by user input
   
   p <- ggplot(df_use, aes(frame = frame)) +
     geom_line(aes(x = Time, y = num_people, color = people_type)) +
@@ -24,24 +31,28 @@ run_animation <- function(df_use) {
     theme(legend.title = element_blank())
   
   
+  # translate static ggplot into an interactive plot
   
   fig <- ggplotly(p)
   
   fig <- fig %>%
     layout (
+    # add x-axis label
       xaxis = list(
         title = "Day",
-        # range = c()
         zeroline = F
       ),
       yaxis = list(
+    # add y-axis label
         title = "Number of People",
         zeroline = F
       ) %>%
-        animation_opts(
-          frame = 2,
-          transition = 0,
-          redraw = T
+      animation_opts(
+    # set animation options
+    # 2 milliseconds between frames
+        frame = 2,
+    # do not redraw plot at end of transition
+        redraw = F
         )
     )
   
