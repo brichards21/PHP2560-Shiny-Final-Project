4a41abbe20058f78a54e3aa0de91fa8925f24bbe
Breanna Richards <brichards21@amherst.edu>
2021-12-11 21:33:56 -0500

    updating run_animation

--- run_animation.R
+++ run_animation.R
@@ -1,20 +1,16 @@
-
 run_animation <- function(df_use) {
   
   wide_dat <- df_use %>%
     pivot_wider(names_from = "people_type", values_from = "num_people")
   
-  max_data <- data.frame(max_p = apply(wide_dat[, -1], 2, max)) 
-  
-  max_data$names <- rownames(max_data)
-  
-  function(dat, var) {
+  accumulate_by <- function(dat, var) {
     var <- lazyeval::f_eval(var, dat)
     lvls <- plotly:::getLevels(var)
     dats <- lapply(seq_along(lvls), function(x) {
       cbind(dat[var %in% lvls[seq(1, x)], ], frame = lvls[[x]])
     })
-    dplyr::bind_rows(dats)
+    return(dplyr::bind_rows(dats))
+    
   }
   
   
@@ -22,22 +18,11 @@ run_animation <- function(df_use) {
   
   
   
-  times <- length(grep(max_data$names, df_use$people_type))
-  
-  df_use <- df_use %>%
-    mutate(max_num = rep(max_data$max_p, times))
-  
-  df_use_check <- df_use %>%
-    filter(num_people %in% max_data$max_p)
-  
-  max_data$max_time <- unique(df_use_check$Time)
-  
-  
-  
   p <- ggplot(df_use, aes(frame = frame)) +
     geom_line(aes(x = Time, y = num_people, color = people_type)) +
     theme_minimal() +
     theme(legend.title = element_blank())
+  
   
   
   fig <- ggplotly(p)
@@ -60,5 +45,9 @@ run_animation <- function(df_use) {
         )
     )
   
+  
+  
+  
+
   
 }
